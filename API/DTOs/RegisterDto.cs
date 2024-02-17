05024221471204d36ff09749ceb1a558cb05724c
Youssef Rami <yousseframi@aucegypt.edu>
2024-02-17 16:03:36 +0200

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,5 +8,6 @@ public class RegisterDto
     public string Username { get; set; }
 
     [Required]
+    [StringLength(maximumLength: 8, MinimumLength = 4)]
     public string Password { get; set; }
 }
